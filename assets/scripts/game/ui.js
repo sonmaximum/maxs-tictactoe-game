bdef5850b8b2c066622b6b99d43de815f1b42106
sonmaximum <maxjuchheim@gmail.com>
2018-02-08 17:04:02 -0500

    Add first game info messages
    Added first messages for invalid move, game over, and turn change

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -2,11 +2,21 @@
 const logic = require('./game_logic.js')
 
 const updateBoard = function (event) {
-  if (!logic.checks.over && !$(event.target).text()) {
+  if (logic.checks.over) {
+    $('#user-feedback-message').text('Game is over!')
+    $('#user-feedback-message').css('background-color', 'red')
+  } else if ($(event.target).text()) {
+    $('#user-feedback-message').text('Invalid move!')
+    $('#user-feedback-message').css('background-color', 'red')
+  } else {
     if (logic.checks.xturn) {
       $(event.target).text(logic.x).addClass('blue-x')
+      $('#user-feedback-message').text('O\'s turn')
+      $('#user-feedback-message').css('background-color', 'white')
     } else {
       $(event.target).text(logic.o).addClass('red-o')
+      $('#user-feedback-message').text('X\'s turn')
+      $('#user-feedback-message').css('background-color', 'white')
     }
   }
 }
