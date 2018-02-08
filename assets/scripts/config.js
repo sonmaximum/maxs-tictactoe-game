e93e669f404e8ed6922699b166e04d7d19b90618
sonmaximum <maxjuchheim@gmail.com>
2018-02-08 09:25:02 -0500

    Set api origins
    Update the api origin urls

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    development: 'http://tic-tac-toe.wdibos.com',
+    production: 'https://aqueous-atoll-85096.herokuapp.com'
   }
 }
 
