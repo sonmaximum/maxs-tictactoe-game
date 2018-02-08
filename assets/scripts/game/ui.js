b05a15fe8c7ecd58476020808df7c2f9d5127bb9
sonmaximum <maxjuchheim@gmail.com>
2018-02-08 16:44:19 -0500

    Style board
    Add some basic styling to the board UI

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -4,9 +4,9 @@ const logic = require('./game_logic.js')
 const updateBoard = function (event) {
   if (!logic.checks.over && !$(event.target).text()) {
     if (logic.checks.xturn) {
-      $(event.target).text(logic.x)
+      $(event.target).text(logic.x).addClass('blue-x')
     } else {
-      $(event.target).text(logic.o)
+      $(event.target).text(logic.o).addClass('red-o')
     }
   }
 }
