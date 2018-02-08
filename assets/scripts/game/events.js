8f7ed3b1532a9be9c71ad3afbff55dce72b5521e
sonmaximum <maxjuchheim@gmail.com>
2018-02-08 13:44:35 -0500

    Refactor game spaces to use ids
    Made game spaces all the same class, and gave them ids

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const addHandlers = () => {
-  $('[class^=space').on('click', function (event) {
-    console.log((event.target).className)
+  $('.space').on('click', function (event) {
+    console.log(event.target.id)
   })
 }
 
