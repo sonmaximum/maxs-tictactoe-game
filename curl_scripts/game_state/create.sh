14f3c7015a2160e2f513c9042b231317871b9b58
sonmaximum <maxjuchheim@gmail.com>
2018-02-09 09:17:05 -0500

    Add create game curl curl_scripts
    Created the curl script to request a new game

---
+++ curl_scripts/game_state/create.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl "http://tic-tac-toe.wdibos.com/games" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{}'
+
+echo
